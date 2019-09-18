953a02964e3d2eca30013b240cb3f37da80ce6e0
Aimee Lewis <aimee.lewis@ufl.edu>
2019-09-18 16:47:36 -0400

    Hope this didn't mess anything up

--- queries.js
+++ queries.js
@@ -1,36 +1,67 @@
 /* Add all the required libraries*/
-
 /* Connect to your database using mongoose - remember to keep your key secret*/
+var mongoose = require('mongoose'),
+  Listing = require('./ListingSchema.js'),
+  config = require('./config.js');
+
 
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
-
 var findLibraryWest = function() {
-  /* 
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
+   Listing.findOne({name: 'Library West'}, function(err, data){
+     if (err) throw err;
+     console.log(data);
+   });
 };
+
+
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This corresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
+   Listing.find({code : 'CABL'}, function(err, data){
+     if(err) throw err;
+      data.remove(function(err){
+        if(err) throw err;
+        console.log(data);
+      });
+  });
 };
+
+
 var updatePhelpsLab = function() {
   /*
-    Phelps Lab address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
-    
+    Phelps Lab address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
+
     Correct Address: 1953 Museum Rd, Gainesville, FL 32603
 
    */
+   Listing.findOne({name: Phelps Laboratory}, function(err, data){
+       if(err) throw err;
+       data.address = '1953 Museum Road Gainesville, FL 32611';
+       data.save(function(err){
+         if (err) throw err;
+         console.log(data);
+       })
+    });
 };
+
+
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
+   Listings.Find{}, function(err, data){
+     if(err) throw err;
+    console.log(data);
+   }
 };
 
 findLibraryWest();
