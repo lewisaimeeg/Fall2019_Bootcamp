18843adddc627f18d820884bf106a9cc431dfddc
Aimee Lewis <aimee.lewis@ufl.edu>
2019-09-19 15:34:20 -0400

    edited config file

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   db: {
-    uri: 'mongodb://lewisaimeeg:c9ju056lv1BIZK40@localhost' //place the URI of your mongo database here.
+    uri: 'mongodb+srv://new_user:myPassWord17@cluster0-sgsfr.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
   }
 };
