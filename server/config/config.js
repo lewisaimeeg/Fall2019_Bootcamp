f7d11e15c84a247c950bbaae0f3f010c30c6d20e
Aimee Lewis <aimee.lewis@ufl.edu>
2019-09-30 15:13:18 -0400

    Changes to main files

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+//This file holds any configuration variables we may need
+//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
+
+module.exports = {
+  db: {
+    uri: ''//place the URI of your mongo database here.
+  },
+  openCage: {
+    key: '' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
+  },
+  port: 8080
+};
--- server/models/listings.server.model.js
+++ server/models/listings.server.model.js
@@ -1,23 +1,47 @@
-//You can replace this entire file with your Bootcamp Assignment #2 - ListingSchema.js File
+//"You can replace this entire file with your Bootcamp Assignment #2 - ListingSchema.js File" -- DONE
+
+// ------------------------------------------------------------------------------
 
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-/* Create your schema */
+/* Create your schema for the data in the listings.json file that will define how data is saved in your database
+     See https://mongoosejs.com/docs/guide.html for examples for creating schemas
+     See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
+  */
+
+//EACH JSON LISTING HAS: CODE, NAME, COORDINATES (LAT, LONG), ADDRESS
 var listingSchema = new Schema({
-  /* your code here from Bootcamp Assignment #2 - ListingSchema.js File*/
-    
+  //Check out - https://mongoosejs.com/docs/guide.html
+  code: {type: String, required: true},
+  name: {type: String, required: true},
+  coordinates: {
+    latitude: Number,
+    longitude: Number
+  },
+  address: String,
+  created_at: Date,
+  updated_at: Date
 });
 
-/* create a 'pre' function that adds the updated_at and created_at if not already there property */
+/* Create a 'pre' function that adds the updated_at (and created_at if not already there) property
+   See https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
+*/
 listingSchema.pre('save', function(next) {
-  /* your code here from Bootcamp Assignment #2 - ListingSchema.js File */
+  //get the current data
+  var currentDate = new Date();
+
+  this.updated_at = currentDate;
+  if(!this.created_at){
+    this.created_at = currentDate;
+  }
 
   next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
+//Check out - https://mongoosejs.com/docs/guide.html#models
 var Listing = mongoose.model('Listing', listingSchema);
 
 /* Export the model to make it avaiable to other parts of your Node application */
